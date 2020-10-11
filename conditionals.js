483ede34c4f06beb6b709ca32c672f9d5bcbbba7
Clarence Chiu <clarchiu@gmail.com>
2020-10-11 06:54:07 +0000

    changed conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,14 @@
-const raining = true;
-const cold = false;
+const isCitizen = true;
+const age = 26;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
 
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
 
-console.log("Now you're ready to go outside!");
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
