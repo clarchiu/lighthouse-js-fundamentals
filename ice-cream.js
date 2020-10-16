20e2ce981548ee2ad887b870d8b219d8b1228f49
Clarence Chiu <clarchiu@gmail.com>
2020-10-16 09:50:37 +0000

    ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push('rootbeer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+console.log(iceCreamFlavours.length);
